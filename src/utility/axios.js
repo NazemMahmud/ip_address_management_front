3277ef4a61723626c48f343fcd67d5854f7943af
Piash <nazem.piash10@gmail.com>
2022-08-03 23:04:59 +0600

    solve refresh token problem

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { appConfig } from "../config/config";
 import { store } from "../redux/store";
+import { handleLogin, handleLogout } from "../redux/authentication";
 
 const guestInstance = axios.create({
    baseURL: appConfig.apiBaseUrl,
@@ -13,7 +14,6 @@ const guestInstance = axios.create({
 
 const authInstance = axios.create({
    baseURL: appConfig.apiBaseUrl,
-   timeout: 10000,
    headers: {
       Accept: "application/json",
       "Content-Type": "application/json"
@@ -34,6 +34,28 @@ authInstance.interceptors.request.use(
     },
     error => Promise.reject(error)
 );
+
+// Response interceptor for API calls in case token mismatch/expires
+authInstance.interceptors.response.use(response => {
+    return response;
+}, async error => {
+    const originalRequest = error?.config;
+    if (error?.response?.status === 401 && !originalRequest?._retry) {
+        originalRequest._retry = true;
+
+        const access_token = error.response.data.data.refresh_token;
+        originalRequest.headers.Authorization = 'Bearer ' + access_token;
+        store.dispatch(handleLogin({ access_token }));
+
+        return authInstance(originalRequest);
+    }
+    // in case token invalid/mismatch but not expired
+    store.dispatch(handleLogout());
+    setTimeout(() => {
+        window.location.href = "/login";
+    }, 1000);
+    await Promise.reject(error);
+});
 
 
 export { guestInstance, authInstance };
